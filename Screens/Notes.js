277070a4f18921896661aeb5b47e1e8d6fd67468
sanimridha <sanimridha307@gmail.com>
2021-03-27 11:49:45 +0600

    init screens

---
+++ Screens/Notes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Notes = () => {
+    return (
+        <View>
+            <Text>Note Screen</Text>
+        </View>
+    );
+};
+
+export default Notes;
+
+const styles = StyleSheet.create({});
---
+++ Screens/Tasks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Tasks = () => {
+    return (
+        <View>
+            <Text>Task Screen</Text>
+        </View>
+    );
+};
+
+export default Tasks;
+
+const styles = StyleSheet.create({});
